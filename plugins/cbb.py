49371848c06d9a30012083e2ee6d26bcc50cdbc2
Veget68 <130899142+Veget68@users.noreply.github.com>
2023-04-26 23:22:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel : @kwicbotupdates\n○ Support Group : @kwicbotupdates</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>@ItachiCR</a>\</a>\/Luffy'>Click here</a>\n○ Channel : @Anime_Hub_7\
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
