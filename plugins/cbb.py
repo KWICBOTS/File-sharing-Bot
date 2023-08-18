1425951057bada0b579731384926119237088751
marsel7official <141441864+marsel7official@users.noreply.github.com>
2023-08-18 21:04:18 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await query.message.edit_text(
             text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel : @kwicbotupdates\n○ Support Group : @kwicbotupdates</b>",
             disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup               [
+            reply_markup = InlineKeyboardMarkup(
+                [
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
